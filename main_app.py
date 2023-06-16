c1c7bc17c1f51ff6ed8d014c96da36ea06cb0fcd
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-16 01:19:25 -0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -3,9 +3,6 @@ import pandas as pd
 import numpy as np
 
 st.title('MOBILI - RASTREAMENTO')
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 
@@ -25,7 +22,6 @@ def load_data(nrows):
     data = load_data2(st.secrets["public_gsheets_url"])
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    #data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
 data_load_state = st.text('Loading data...')
@@ -37,12 +33,12 @@ if st.checkbox('Show raw data'):
     st.write(data)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+#hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 # Some number in the range 0-23
 hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+#filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
