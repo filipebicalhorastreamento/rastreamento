2fe48c463f2a4294558f92161fe47cde94802adc
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-16 00:59:56 -0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -14,8 +14,8 @@ def load_data(sheets_url):
 df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+#for row in df.itertuples():
+    #st.write(f"{row.name} has a :{row.pet}:")
 
 
 """@st.cache_data
