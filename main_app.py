f6afb09bb0965fc2ffe034125e6c4373160af4b1
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-16 01:31:36 -0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -17,16 +17,16 @@ def load_data2(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
-data_load_state = st.text('CARREGANDO.')
+#data_load_state = st.text('CARREGANDO.')
 data = load_data(10000)
-data_load_state.text("CONCLUÍDO")
+#data_load_state.text("CONCLUÍDO")
 
 if st.checkbox('Mostrar dados'):
     st.subheader('Dataframe')
     st.write(data)
 
 st.subheader('Veículos por situação.')
-#hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 # Some number in the range 0-23
