1a0f01fea0d644879280fb1a3a67c38e2822a245
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-16 00:59:23 -0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -24,7 +24,7 @@ def load_data(nrows):
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    return data"""
 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
@@ -43,4 +43,4 @@ hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)"""
+st.map(filtered_data)
