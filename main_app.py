b0147ebb95863c1d50c7abd28d505006992ff51e
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-16 03:06:18 -0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -22,10 +22,11 @@ data = load_data(10000)
 
 st.subheader('Veículos por situação.')
 situações = data['SITUAÇÃO'].value_counts()
-
+situações = situações.rename(columns={'count': 'QUANTIDADE'})
+situações_invertido = situações.T
 st.bar_chart(situações)
 st.table(situações)
-st.write(situações)
+st.write(situações_invertido)
 # Some number in the range 0-23
 hour_to_filter = st.slider('hour', 0, 23, 17)
 #filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
