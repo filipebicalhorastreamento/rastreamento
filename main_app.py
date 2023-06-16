710cb02eb844f42198cfe44fe52d0718c6bc5161
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-16 03:44:44 -0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -35,7 +35,7 @@ st.subheader('LISTA DE VEÍCULOS')
 col1, col2 = st.columns([3, 1])
 col1.subheader("A wide column with a chart")
 # Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
+hour_to_filter = col1.slider('hour', 0, 23, 17)
 col2.subheader("A wide column with a chart")
 col2.write(data)
 #filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
