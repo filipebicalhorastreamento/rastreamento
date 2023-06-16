2daf313dc8afce02a90a3577bc7e0cebd3eab70f
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-16 01:08:14 -0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -8,12 +8,12 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-"""@st.cache_data(ttl=600)
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
+#@st.cache_data(ttl=600)
+#def load_data(sheets_url):
+#    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+#    return pd.read_csv(csv_url)
 
-df = load_data(st.secrets["public_gsheets_url"])
+#df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
 #for row in df.itertuples():
