be554a0348632c271081d0a7260df3f338c8e544
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-18 03:53:57 -0300

    Update httprequest.py

--- pages/httprequest.py
+++ pages/httprequest.py
@@ -1,6 +1,33 @@
 import requests
 import streamlit as st
 
-data = requests.get("'https://jsonplaceholder.typicode.com/todos/1'").json()
+# Define as informações de autenticação
+auth_url = 'https://sgr.hinova.com.br/sgr/sgrv2_api/service_api/servicos'
+auth_payload = {
+    'COD': 'seu_codigo',
+    'CLIENTE': 'seu_cliente',
+    'SENHA': 'sua_senha'
+}
 
-st.write(data)
+# Faz a solicitação de autenticação
+response = requests.post(auth_url, json=auth_payload)
+data = response.json()
+
+# Verifica se a autenticação foi bem-sucedida
+if response.status_code == 200 and data.get('Error') == 'false':
+    # Obtém o token de autenticação
+    auth_token = data['Headers']['X-Auth-Token']
+    headers = {'Authorization': auth_token}
+
+    # Faz uma solicitação GET para buscar um veículo específico
+    chave_api = 'sua_chave_api'
+    veiculo_url = f'https://sgr.hinova.com.br/sgr/sgrv2_api/service_api/buscar_veiculo/{chave_api}'
+    veiculo_response = requests.get(veiculo_url, headers=headers)
+    veiculo_data = veiculo_response.json()
+
+    # Exibe os dados no Streamlit
+    st.subheader('Dados do Veículo')
+    st.write(veiculo_data)
+else:
+    st.error('Erro na autenticação. Verifique as credenciais.')
+
