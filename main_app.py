be322e8e5cf41901fb8a6cce5536351eea0cc59a
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-18 03:03:03 -0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -42,9 +42,9 @@ st.subheader('Gráfico de Pizza')
 c = alt.Chart(situações_pizza).mark_arc(innerRadius=50).encode(
     theta=alt.Theta(field="count", type="quantitative"),
     color=alt.Color(field="Situação", type="nominal")
-"""
-#situações = situações[["SITUAÇÃO", "count"]]
 
+#situações = situações[["SITUAÇÃO", "count"]]
+"""
 situações_inv = situações.T
 c = alt.Chart(situações).mark_arc(innerRadius=50).encode(
     theta=alt.Theta(field="count", type="quantitative"),
