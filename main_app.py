204603d0d9342f27b083f1a7d7a0c85bf83fde3b
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-18 03:38:21 -0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -16,12 +16,11 @@ import streamlit as st
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
-st.set_page_config(layout="wide")
-
 
 def run():
     st.set_page_config(
         page_title="MOBILI - RASTREAMENTO",
+        layout="wide",
     )
 
     st.write("MOBILI - RASTREAMENTO")
