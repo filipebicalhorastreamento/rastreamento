bf70914d0f827ed03d75adc7bed6ad7ac12c6b3c
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-18 00:46:13 -0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -24,7 +24,7 @@ def load_data2(sheets_url):
 data = load_data(10000)
 data_frame = data
 data_frame ['DATA/HORA ALTERAÇÃO'] = pd.to_datetime(data_frame['DATA/HORA ALTERAÇÃO']).dt.date
-situações = data['SITUAÇÃO'].value_counts().to_frame()
+situações = data_frame['SITUAÇÃO'].value_counts().to_frame()
 situações_inv = situações.T
 
 st.subheader('SITUAÇÕES')
