4a5fef5355ad66d40f258f7dc7818ed82c69389b
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-19 17:42:32 -0300

    Update ssx.py

--- pages/ssx.py
+++ pages/ssx.py
@@ -1,4 +1,4 @@
-import requests
+"""import requests
 
 url = "https://integration.systemsatx.com.br/Login?Username=filipebicalho.rastreamento%40gmail.com&Password=A%40sU8e1Gzl&Hashcentral=ACB2CC74-D4E2-4835-9E49-41109C43D6CB"
 
@@ -7,4 +7,4 @@ headers = {}
 
 response = requests.request("POST", url, headers=headers, data=payload)
 
-print(response.text)
+print(response.text)"""
