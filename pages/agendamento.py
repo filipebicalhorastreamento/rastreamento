a50dc340142e50384b16cbd0d63aefea78d5a697
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-19 17:44:16 -0300

    Update agendamento.py

--- pages/agendamento.py
+++ pages/agendamento.py
@@ -4,7 +4,7 @@ from datetime import date
 import pandas as pd
 import numpy as np
 import altair as alt
-
+import requests
 st.set_page_config(layout="wide")
 st.title('MOBILI - AGENDAMENTO')
 
@@ -13,10 +13,29 @@ st.title('MOBILI - AGENDAMENTO')
 
 @st.cache_data
 def load_data(nrows):
-    data = load_data2(st.secrets["public_gsheets_url"])
-    uppercase = lambda x: str(x).upper()
-    data.rename(uppercase, axis='columns', inplace=True)
-    return data
+
+    url = "https://sgr.hinova.com.br/sgr/sgrv2_api/service_api/servicos/headers_authorization?cliente=3542&nome=operacional&senha=WR3D5K"
+
+    payload = {}
+    headers = {}
+
+    session = requests.Session()
+    response = session.post(url=url, headers=headers, data=payload)
+    data = response.json()
+
+    # Verifica se a autenticação foi bem-sucedida
+    if response.status_code == 200 and data.get('error') == False:
+        # Faz uma solicitação GET para buscar um veículo específico
+        chave_api = 'c5b79e7ce0c72d6e3c9842a51433c726'
+        veiculo_url = f'https://sgr.hinova.com.br/sgr/sgrv2_api/service_api/servicos/buscar_veiculo/{chave_api}'
+        veiculo_response = session.get(url=veiculo_url)
+        data = veiculo_response.json()
+
+        # Exibe os dados no Streamlit
+        return data
+    else:
+        print('Erro na autenticação. Verifique as credenciais.')
+        return data
 
 def load_data2(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
