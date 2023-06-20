9a950c262c5eb8f90d4a24b127b5d4d6212742d8
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-19 23:02:36 -0300

    Update httprequest.py

--- pages/httprequest.py
+++ pages/httprequest.py
@@ -58,13 +58,13 @@ c = alt.Chart(situações_pizza).mark_arc(innerRadius=50).encode(
     theta=alt.Theta(field="Count", type="quantitative"),
     color=alt.Color(field="Situação", type="nominal"),
 )
-st.dataframe(situações,use_container_width=True ,hide_index=True)
+st.dataframe(situações.T,use_container_width=True ,hide_index=True)
 st.altair_chart(c, use_container_width=True)
 
 st.subheader('LISTA DE VEÍCULOS POR SITUAÇÃO')
 col1, col2 = st.columns([1, 5])
 
-situação_filtro = col1.sidebar.selectbox(
+situação_filtro = st.sidebar.selectbox(
     "Situação",
     ('AGENDADO',
     'ATIVO',
