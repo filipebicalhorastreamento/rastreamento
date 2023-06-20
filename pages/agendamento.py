7b59ec278904c90ff5337c39aa2bf59480ffa04b
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-19 21:48:09 -0300

    Update agendamento.py

--- pages/agendamento.py
+++ pages/agendamento.py
@@ -51,17 +51,70 @@ def load_data(nrows):
         print('Erro na autenticação. Verifique as credenciais.')
     return dados
 
-def load_data2(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
-
 dados = load_data(10000000)
 
 data_frame = pd.DataFrame.from_dict(dados)
 
+data_frame ['DATA/HORA ALTERAÇÃO'] = pd.to_datetime(data_frame['DATA/HORA ALTERAÇÃO']).dt.date
+situações = data_frame
+situações = situações['SITUAÇÃO'].value_counts().to_frame()
+situações_inv = situações.T
+
+# Criar um novo DataFrame para o gráfico de pizza
+situações_pizza = pd.DataFrame({'Situação': situações.index, 'Count': situações['count']})
+
+
+# Plotar o gráfico de pizza usando o novo DataFrame
+st.subheader('SITUAÇÕES')
+c = alt.Chart(situações_pizza).mark_arc(innerRadius=50).encode(
+    theta=alt.Theta(field="Count", type="quantitative"),
+    color=alt.Color(field="Situação", type="nominal"),
+)
+st.dataframe(situações_pizza.T,use_container_width=True ,hide_index=True)
+st.altair_chart(c, use_container_width=True)
+
+
+st.subheader('LISTA DE VEÍCULOS POR SITUAÇÃO')
+col1, col2 = st.columns([1, 5])
+
+f_date = date.today()
+data_frame['DATA SITUAÇÃO'] = data_frame['DATA/HORA ALTERAÇÃO']
+data_frame['NÚMERO DE DIAS'] = (f_date - data_frame['DATA SITUAÇÃO']) / np.timedelta64(1, 'D')
+data_frame = data[["NOME", "PLACA", "SITUAÇÃO","DATA SITUAÇÃO", "CIDADE CLIENTE", "ESTADO CLIENTE", "NÚMERO DE DIAS", "OBSERVAÇÃO"]]
+
+situação_filtro = st.sidebar.selectbox(
+    "Situação",
+    ('AGENDADO',
+    'ATIVO',
+    'INATIVO',
+    'MANUTENÇÃO',
+    'MUZZI',
+    'PENDENTE',
+    'PENDENTE INSTALAÇÃO',
+    'PROPRIO',
+    'RECUSADO',
+    'RETIRADA',
+    'SAFECAR'))
+
+
+uf = data_frame['ESTADO CLIENTE'].unique()
+make_choice = st.sidebar.selectbox('Select your vehicle:', uf)
+
+remover_filtro = st.sidebar.checkbox("Remover filtros")
+
+if remover_filtro:
+    filtered_data = data_frame
+else:
+    selecao = (data_frame['SITUAÇÃO'] == situação_filtro) & (data_frame['ESTADO CLIENTE'] == make_choice)
+    filtered_data = data_frame[selecao]
+    
+estado = filtered_data['ESTADO CLIENTE'].value_counts().to_frame()
+col1.dataframe(data=estado, use_container_width=True, hide_index=False)
+col2.dataframe(data=filtered_data, use_container_width=True, hide_index=True)
+
 st.subheader('DADOS BRUTOS PARA CONFERÊNCIA')
 if st.checkbox('Mostrar dados'):
     st.subheader('Dataframe')
-    st.write(dados)
-    st.write(data_frame)
+    st.write(data)
+
 
