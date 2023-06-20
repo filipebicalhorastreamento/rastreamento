d0d01cb63992caf914b9afb51a217d36dbc979ce
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-19 22:56:39 -0300

    Update httprequest.py

--- pages/httprequest.py
+++ pages/httprequest.py
@@ -1,23 +1,50 @@
 import requests
+st.set_page_config(layout="wide")
+st.title('MOBILI - VEÍCULOS POR SITUAÇÃO')
 
-url = "https://sgr.hinova.com.br/sgr/sgrv2_api/service_api/servicos/headers_authorization?cliente=3542&nome=operacional&senha=WR3D5K"
+chave_api = 'c5b79e7ce0c72d6e3c9842a51433c726'
 
-payload = {}
-headers = {}
+@st.cache_data
+def load_data(nrows):
 
-session = requests.Session()
-response = session.post(url=url, headers=headers, data=payload)
-data = response.json()
+    url = "https://sgr.hinova.com.br/sgr/sgrv2_api/service_api/servicos/headers_authorization?cliente=3542&nome=operacional&senha=WR3D5K"
 
-# Verifica se a autenticação foi bem-sucedida
-if response.status_code == 200 and data.get('error') == False:
-    # Faz uma solicitação GET para buscar um veículo específico
-    chave_api = 'c5b79e7ce0c72d6e3c9842a51433c726'
-    veiculo_url = f'https://sgr.hinova.com.br/sgr/sgrv2_api/service_api/servicos/buscar_veiculo/{chave_api}'
-    veiculo_response = session.get(url=veiculo_url)
-    veiculo_data = veiculo_response.json()
+    payload = {}
+    headers = {}
 
-    # Exibe os dados no Streamlit
-    st.write(veiculo_data)
-else:
-    print('Erro na autenticação. Verifique as credenciais.')
+    session = requests.Session()
+    response = session.post(url=url, headers=headers, data=payload)
+    data = response.json()
+
+    # Verifica se a autenticação foi bem-sucedida
+    if response.status_code == 200 and data.get('error') == False:
+        dados = []
+        indice = 0
+
+        while True:
+            # Faz uma solicitação GET para buscar um veículo específico
+            chave_api = 'c5b79e7ce0c72d6e3c9842a51433c726'
+            veiculo_url = f'https://sgr.hinova.com.br/sgr/sgrv2_api/service_api/servicos/buscar_veiculo/{chave_api}'
+            params = {'indice': str(indice)}
+            veiculo_response = session.get(url=veiculo_url, params=params)
+            veiculo_data = veiculo_response.json()
+
+            # Se dar algum erro, ou não retonar 200, quebrar o loop pois chegou no fim da lista
+            if veiculo_response.status_code != 200 or veiculo_data.get('error') == True or len(veiculo_data.get('data')) == 0:
+                break
+
+        # Adicionar dados na lista de dados
+            dados.extend(veiculo_data.get('data'))
+
+        # Subir indice, para procurar no proximo indice
+            indice += 200
+
+        # Exibe os dados no Streamlit
+        return dados
+    else:
+        print('Erro na autenticação. Verifique as credenciais.')
+    return dados
+
+dados = load_data(10000000)
+df = pd.DataFrame.from_dict(dados)
+st.dataframe(df)
