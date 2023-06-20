29340805b7bcfec4fde2ceb286cfc41077c5d61d
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-19 22:11:23 -0300

    Update Veiculo por Situação.py

--- pages/Veiculo por Situação.py
+++ pages/Veiculo por Situação.py
@@ -6,7 +6,7 @@ import numpy as np
 import altair as alt
 import requests
 st.set_page_config(layout="wide")
-st.title('MOBILI - AGENDAMENTO')
+st.title('MOBILI - VEÍCULOS POR SITUAÇÃO')
 
 chave_api = 'c5b79e7ce0c72d6e3c9842a51433c726'
 
@@ -60,13 +60,51 @@ situações = situações['situacao_veiculo'].value_counts().to_frame()
 situações_inv = situações.T
 situações_pizza = pd.DataFrame({'Situação': situações.index, 'Count': situações['count']})
 
-st.subheader('SITUAÇÕES')
+st.subheader('SITUAÇÕES ATIVAS')
 c = alt.Chart(situações_pizza).mark_arc(innerRadius=50).encode(
     theta=alt.Theta(field="Count", type="quantitative"),
     color=alt.Color(field="Situação", type="nominal"),
 )
 st.dataframe(situações_pizza.T,use_container_width=True ,hide_index=True)
 st.altair_chart(c, use_container_width=True)
+
+st.subheader('LISTA DE VEÍCULOS POR SITUAÇÃO')
+col1, col2 = st.columns([1, 5])
+
+f_date = date.today()
+data_frame['DATA SITUAÇÃO'] = data_frame['ultima_atualizacao']
+data_frame['NÚMERO DE DIAS'] = (f_date - data_frame['DATA SITUAÇÃO']) / np.timedelta64(1, 'D')
+data_frame = data[["NOME", "PLACA", "SITUAÇÃO","DATA SITUAÇÃO", "CIDADE CLIENTE", "ESTADO CLIENTE", "NÚMERO DE DIAS", "OBSERVAÇÃO"]]
+
+situação_filtro = st.sidebar.selectbox(
+    "Situação",
+    ('AGENDADO',
+    'ATIVO',
+    'INATIVO',
+    'MANUTENÇÃO',
+    'MUZZI',
+    'PENDENTE',
+    'PENDENTE INSTALAÇÃO',
+    'PROPRIO',
+    'RECUSADO',
+    'RETIRADA',
+    'SAFECAR'))
+
+
+uf = data_frame['ESTADO CLIENTE'].unique()
+make_choice = st.sidebar.selectbox('Select your vehicle:', uf)
+
+remover_filtro = st.sidebar.checkbox("Remover filtros")
+
+if remover_filtro:
+    filtered_data = data_frame
+else:
+    selecao = (data_frame['SITUAÇÃO'] == situação_filtro) & (data_frame['ESTADO CLIENTE'] == make_choice)
+    filtered_data = data_frame[selecao]
+    
+estado = filtered_data['ESTADO CLIENTE'].value_counts().to_frame()
+col1.dataframe(data=estado, use_container_width=True, hide_index=False)
+col2.dataframe(data=filtered_data, use_container_width=True, hide_index=True)
 
 st.subheader('DADOS BRUTOS PARA CONFERÊNCIA')
 if st.checkbox('Mostrar dados'):
