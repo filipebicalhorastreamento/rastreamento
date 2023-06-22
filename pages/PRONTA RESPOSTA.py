46d2c759ba5f7c335d4dd32507d09d2e02af4cae
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-22 14:21:23 -0300

    Update PRONTA RESPOSTA.py

--- pages/PRONTA RESPOSTA.py
+++ pages/PRONTA RESPOSTA.py
@@ -28,11 +28,11 @@ data_frame = pd.DataFrame(dados)
 data_frame ['DATA'] = pd.to_datetime(data_frame ['DATA']).dt.date
 
 estado = data_frame['ESTADO'].value_counts().to_frame()
-estado_count = pd.DataFrame({'estado': estado.index, 'Count': estado['count']})
+estado_count = pd.DataFrame({'ESTADO': estado.index, 'Count': estado['count']})
 
 c = alt.Chart(estado_count).mark_arc(innerRadius=50).encode(
     theta=alt.Theta(field="QUANTIDADE", type="quantitative"),
-    color=alt.Color(field="ESTADO", type="nominal"),
+    color=alt.Color(field="estado", type="nominal"),
 )
 
 col1, col2, col3 = st.columns([3, 3, 3])
