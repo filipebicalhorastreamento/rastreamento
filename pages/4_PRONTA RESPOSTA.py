dd66ef73b5e28e2f7c9a6d82002e79976a174c34
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-22 17:36:43 -0300

    Update 4_PRONTA RESPOSTA.py

--- pages/4_PRONTA RESPOSTA.py
+++ pages/4_PRONTA RESPOSTA.py
@@ -6,8 +6,7 @@ import numpy as np
 import altair as alt
 
 st.set_page_config(layout="wide")
-st.title('MOBILI - RASTREAMENTO')
-st.subheader('PRONTA RESPOSTA')
+st.title('PRONTA RESPOSTA')
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 
@@ -36,6 +35,7 @@ c = alt.Chart(estado_count).mark_arc(innerRadius=50).encode(
 )
 
 col1, col2, col3 = st.columns([3, 3, 3])
+col1.subheader('POR ESTADO')
 col1.altair_chart(c, use_container_width=True)
 
 
