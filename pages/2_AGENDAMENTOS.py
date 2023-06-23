8784965ceabfab1855784d1f83fba4b1baedd6e3
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-22 22:07:28 -0300

    Update 2_AGENDAMENTOS.py

--- pages/2_AGENDAMENTOS.py
+++ pages/2_AGENDAMENTOS.py
@@ -54,6 +54,6 @@ def load_data(nrows):
 
 dados = load_data(10000000)
 df = pd.DataFrame.from_dict(dados)
-nova_ordem = ['servico', 'contratante', 'placa', 'situacao', 'tecnico_telefone', 'data_inicial', 'Numero_dias', 'cidade', 'uf']
+nova_ordem = ['servico']
 agendamento = df[nova_ordem]
 st.dataframe(agendamento)
