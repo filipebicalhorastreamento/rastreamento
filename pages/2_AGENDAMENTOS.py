9ca6205b7b220a6a130536fa1fcc21584060be1b
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-22 22:36:02 -0300

    Update 2_AGENDAMENTOS.py

--- pages/2_AGENDAMENTOS.py
+++ pages/2_AGENDAMENTOS.py
@@ -74,7 +74,7 @@ c2 = alt.Chart(df_servico).mark_arc(innerRadius=50).encode(
 col1.subheader('Por Estado')
 col1.dataframe(df_uf,use_container_width=True ,hide_index=True)
 col2.altair_chart(c, use_container_width=True)
-col1.subheader('Por Serviço')
+col3.subheader('Por Serviço')
 col3.dataframe(servico,use_container_width=True ,hide_index=True)
 col4.altair_chart(c2, use_container_width=True)
 
