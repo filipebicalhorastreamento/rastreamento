b542b55efd400e056f921afad9c98da76d43e205
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-22 22:56:09 -0300

    Update 3_TECNICOS CREDENCIADOS.py

--- pages/3_TECNICOS CREDENCIADOS.py
+++ pages/3_TECNICOS CREDENCIADOS.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import datetime
-from datetime import date
 import pandas as pd
-import numpy as np
-import altair as alt
+import folium
+from streamlit_folium import st_folium
 
 st.set_page_config(layout="wide")
 st.title('MOBILI - RASTREAMENTO')
@@ -26,3 +24,13 @@ dados = load_data(10000000)
 
 data_frame = pd.DataFrame(dados)
 st.dataframe(data_frame)
+
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
