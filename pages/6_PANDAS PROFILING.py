f7008b85a8398ef3ef530426700bdd4f9f4a5aaa
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-25 02:42:40 -0300

    Update 6_PANDAS PROFILING.py

--- pages/6_PANDAS PROFILING.py
+++ pages/6_PANDAS PROFILING.py
@@ -4,7 +4,18 @@ import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
 
-df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
+def load_data(nrows):
+    data = load_data2(st.secrets["public_gsheets_url2"])
+    uppercase = lambda x: str(x).upper()
+    data.rename(uppercase, axis='columns', inplace=True)
+    return data
+
+def load_data2(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+dados = load_data(10000000)
+df = pd.DataFrame(dados)
 pr = df.profile_report()
 
 st_profile_report(pr)
