192545c82fc8736fa859afece416e5fa6cc189b0
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-28 12:49:34 -0300

    Update 6_PANDAS PROFILING.py

--- pages/6_PANDAS PROFILING.py
+++ pages/6_PANDAS PROFILING.py
@@ -5,7 +5,7 @@ import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
 
 def load_data(nrows):
-    data = load_data2(st.secrets["public_gsheets_url2"])
+    data = load_data2(st.secrets["public_gsheets_url3"])
     uppercase = lambda x: str(x).upper()
     data.rename(uppercase, axis='columns', inplace=True)
     return data
