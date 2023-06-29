85b2199895ce7bea0d985b710a96d8dc382e738d
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-28 23:00:46 -0300

    Update TELA INICIAL.py

--- TELA INICIAL.py
+++ TELA INICIAL.py
@@ -1,79 +1,28 @@
 import streamlit as st
-import requests
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
 
-@st.cache_data
-def load_data(nrows):
+st.write("# Welcome to Streamlit! 👋")
 
-    url = "https://sgr.hinova.com.br/sgr/sgrv2_api/service_api/servicos/headers_authorization?cliente=3542&nome=operacional&senha=WR3D5K"
+st.sidebar.success("Select a demo above.")
 
-    payload = {}
-    headers = {}
-
-    session = requests.Session()
-    response = session.post(url=url, headers=headers, data=payload)
-    data = response.json()
-
-    # Verifica se a autenticação foi bem-sucedida
-    if response.status_code == 200 and data.get('error') == False:
-        dados = []
-        indice = 0
-
-        while True:
-            # Faz uma solicitação GET para buscar um veículo específico
-            chave_api = 'c5b79e7ce0c72d6e3c9842a51433c726'
-            veiculo_url = f'https://sgr.hinova.com.br/sgr/sgrv2_api/service_api/servicos/buscar_veiculo/{chave_api}'
-            params = {'indice': str(indice)}
-            veiculo_response = session.get(url=veiculo_url, params=params)
-            veiculo_data = veiculo_response.json()
-
-            # Se dar algum erro, ou não retonar 200, quebrar o loop pois chegou no fim da lista
-            if veiculo_response.status_code != 200 or veiculo_data.get('error') == True or len(veiculo_data.get('data')) == 0:
-                break
-
-        # Adicionar dados na lista de dados
-            dados.extend(veiculo_data.get('data'))
-
-        # Subir indice, para procurar no proximo indice
-            indice += 200
-
-        # Exibe os dados no Streamlit
-        return dados
-    else:
-        print('Erro na autenticação. Verifique as credenciais.')
-    return dados
-
-
-def run():
-    st.set_page_config(
-        page_title="MOBILI - RASTREAMENTO",
-        layout="wide",
-    )
-
-    st.header("MOBILI - RASTREAMENTO")
-
-    st.sidebar.success("Carregado com sucesso!")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+st.markdown(
     """
-    )
-
-
-if __name__ == "__main__":
-    run()
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
