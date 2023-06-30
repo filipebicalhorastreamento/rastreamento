cfd434b3e622a46807aed576b7fd13ca9f6418fe
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-30 03:50:01 -0300

    Update 9_ENVIO DE EMAILS.py

--- pages/9_ENVIO DE EMAILS.py
+++ pages/9_ENVIO DE EMAILS.py
@@ -37,7 +37,7 @@ recipient = st.text_input("Destinatário")
 if st.button("Enviar"):
     try:
         send_email(subject, body, recipient)
-    st.success("E-mail enviado com sucesso!")
+        st.success("E-mail enviado com sucesso!")
     except (gaierror, ConnectionRefusedError):
         print('Failed to connect to the server. Bad connection settings?')
     except smtplib.SMTPServerDisconnected:
