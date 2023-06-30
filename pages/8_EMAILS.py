58e0d1d080f447639989c69cdb59660bebbb03c1
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-30 09:15:49 -0300

    Update 8_EMAILS.py

--- pages/8_EMAILS.py
+++ pages/8_EMAILS.py
@@ -7,16 +7,16 @@ col1.code('''
     ***Atenção: Aviso de Veículos Desatualizados***
 
     Veículos Desatualizados:
-    Prezado cliente, segue a lista de 3 veículo(s) sem atualização há pelo menos 96 horas.
+    Prezado [nome_associado], seu veículo de placa: [placa] está sem atualização há pelo menos 96 horas.
 
     A desatualização de veículos pode ser ocasionada pelos motivos abaixo:
 
-    Fora da cobertura de GSM (celular)
-    Desligado desde a ultima atualização
-    Em manutenção.
-    Equipamento com problema.
+    1. Fora da cobertura de GSM (celular)
+    2. Desligado desde a ultima atualização
+    3. Veículo em manutenção.
+    4. Equipamento com problema.
 
-    Caso o seu veículo não esteja em nenhuma das 3 primeiras situações, entre em contato pelo telefone (31) 2535-6060 [ ON SIG RASTREAMENTO ] solicitando uma verificação do equipamento.
+    Caso o seu veículo não esteja em nenhuma das 3 primeiras situações, entre em contato pelo telefone (31) 2515-1616 ou através do Mobili Express (31) 99940-9900 solicitando uma verificação do equipamento.
     ''')
 
 col2.write('Contato sem sucesso.')
