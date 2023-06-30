43d07211aa159bd24bc017e41152841632991cda
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-30 02:10:36 -0300

    Update 0_VEICULOS ATIVOS.py

--- pages/0_VEICULOS ATIVOS.py
+++ pages/0_VEICULOS ATIVOS.py
@@ -95,7 +95,7 @@ dfbase['ÚLTIMA CONEXÃO COM O SERVIDOR'] = pd.to_datetime(dfbase['ÚLTIMA CONEX
 dfbase['DATA GPS'] = pd.to_datetime(dfbase['DATA GPS'],dayfirst=True)
 
 dfbase['ultima_transmissao'] = dfbase['ÚLTIMA TRANSMISSÃO'].fillna(dfbase['ÚLTIMA CONEXÃO COM O SERVIDOR']).fillna(dfbase['DATA GPS'])
-df['Nº DIAS'] = (f_date - df['DATA SITUAÇÃO']) / np.timedelta64(1, 'D')
+dfbase['Nº DIAS'] = (f_date - dfbase['ultima_transmissao']) / np.timedelta64(1, 'D')
 dfbase['ultima_transmissao'] = pd.to_datetime(dfbase['ultima_transmissao'].dt.strftime('%d-%m-%Y %H:%M:%S'))
 
 dfbase['plataforma'] = np.select(conditions, values, default='')
