d8a0833f296196dabe4a3c43a042faf52d69e141
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-30 03:09:12 -0300

    Update 8_EMAILS.py

--- pages/8_EMAILS.py
+++ pages/8_EMAILS.py
@@ -1,17 +1,4 @@
 import streamlit as st
-Atenção: Aviso de Veículos Desatualizados
-
-Veículos Desatualizados:
-Prezado cliente, segue a lista de 3 veículo(s) sem atualização há pelo menos 96 horas.
-
-A desatualização de veículos pode ser ocasionada pelos motivos abaixo:
-
-Fora da cobertura de GSM (celular)
-Desligado desde a ultima atualização
-Em manutenção.
-Equipamento com problema.
-
-Caso o seu veículo não esteja em nenhuma das 3 primeiras situações, entre em contato pelo telefone (31) 2535-6060 [ ON SIG RASTREAMENTO ] solicitando uma verificação do equipamento.
 st.title('PROCEDIMENTO OPERACIONAL PADRÃO')
     st.markdown(
         """
