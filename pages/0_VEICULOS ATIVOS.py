9f80cb75921572b111fa8a394b50dd4ccb04534c
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-06-29 22:38:23 -0300

    Update 0_VEICULOS ATIVOS.py

--- pages/0_VEICULOS ATIVOS.py
+++ pages/0_VEICULOS ATIVOS.py
@@ -29,32 +29,12 @@ data_frame ['DATA/HORA ALTERAÇÃO'] = pd.to_datetime(data_frame['DATA/HORA ALTE
 estado = data_frame['ESTADO CLIENTE'].value_counts().to_frame()
 estado_count = pd.DataFrame({'ESTADO': estado.index, 'Count': estado['count']})
 
-FIPE = data_frame['FIPE'].value_counts().to_frame()
-estado_count = pd.DataFrame({'ESTADO': estado.index, 'Count': estado['count']})
-
-estado = data_frame['ESTADO'].value_counts().to_frame()
-estado_count = pd.DataFrame({'ESTADO': estado.index, 'Count': estado['count']})
-
 c = alt.Chart(estado_count).mark_arc(innerRadius=50).encode(
     theta=alt.Theta(field="Count", type="quantitative"),
     color=alt.Color(field="ESTADO", type="nominal"),
 )
-c2 = alt.Chart(data_frame).mark_arc(innerRadius=50).encode(
-    theta=alt.Theta(field="Count", type="quantitative"),
-    color=alt.Color(field="ESTADO", type="nominal"),
-)
-c3 = alt.Chart(data_frame).mark_arc(innerRadius=50).encode(
-    theta=alt.Theta(field="Count", type="quantitative"),
-    color=alt.Color(field="FIPE", type="nominal"),
-)
 
-col1, col2, col3 = st.columns([3, 3, 3])
-col1.subheader('POR ESTADO')
-col1.altair_chart(c, use_container_width=True)
-col2.subheader('POR FIPE')
-col2.altair_chart(c2, use_container_width=True)
-col3.subheader('POR MES')
-col3.altair_chart(c3, use_container_width=True)
-
-
+#col1, col2, col3 = st.columns([3, 3, 3])
+st.subheader('POR ESTADO')
+st.altair_chart(c, use_container_width=True)
 st.dataframe(data=data_frame, use_container_width=True, hide_index=True)
