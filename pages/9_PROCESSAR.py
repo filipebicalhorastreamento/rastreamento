bd4bc31c3dbdaf3532c9864854dd10c1d254c9c7
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-07-04 15:15:51 -0300

    Update 9_PROCESSAR.py

--- pages/9_PROCESSAR.py
+++ pages/9_PROCESSAR.py
@@ -7,9 +7,8 @@ if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
-
-   """ # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    # To convert to a string based IO:
+    """stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     st.write(stringio)
 
     # To read file as string:
