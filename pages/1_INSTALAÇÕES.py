8106489e8284de50ed005c4fbbbb8398b4072320
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2023-07-05 12:14:01 -0300

    Update 1_INSTALAÇÕES.py

--- pages/1_INSTALAÇÕES.py
+++ pages/1_INSTALAÇÕES.py
@@ -65,7 +65,8 @@ dadossheets = load_sheets(10000000)
 dados = load_data(10000000)
 dfagendamentos = pd.DataFrame.from_dict(dadossheets)
 dfinstalacoes = pd.DataFrame.from_dict(dados)
-nova_ordem = dfinstalacoes[["placa_veiculo", "nome_cliente","situacao_veiculo","modelo_veiculo","cidade_veiculo","uf_veiculo","ultima_atualizacao"]]
+dfinstalacoes ['Whatsapp'] = df['dfinstalacoes'] + 'https://web.whatsapp.com/' + df['placa_veiculo']
+nova_ordem = dfinstalacoes[["placa_veiculo", "nome_cliente","situacao_veiculo","modelo_veiculo","cidade_veiculo","uf_veiculo","ultima_atualizacao","Whatsapp"]]
 dfinstalacoes = nova_ordem
 
 filtro = (dfinstalacoes['situacao_veiculo'] == 'PENDENTE INSTALAÇÃO') | (dfinstalacoes['situacao_veiculo'] == 'AGENDADO') | (dfinstalacoes['situacao_veiculo'] == 'RECUSADO')
