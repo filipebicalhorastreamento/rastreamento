548d0e1ce90756c4ae802f30ac4549f419fa6cd8
filipebicalhorastreamento <131942667+filipebicalhorastreamento@users.noreply.github.com>
2024-08-04 01:46:27 -0300

    Add files via upload

---
+++ fleet/framework/config/mail.php
@@ -0,0 +1,23 @@
+<?php
+/*
+@copyright
+
+Fleet Manager v6.1
+
+Copyright (C) 2017-2022 Hyvikk Solutions <https://hyvikk.com/> All rights reserved.
+Design and developed by Hyvikk Solutions <https://hyvikk.com/>
+
+ */
+// save your email config here:
+return array(
+    "driver" => "YOUR_MAIL_DRIVER",
+    "host" => "YOUR_MAIL_HOST",
+    "port" => 2525, // YOUR_MAIL_PORT
+    "from" => array(
+        "address" => "from@example.com", // FROM_EMAIL_ADDRESS
+        "name" => "Example", // FROM_USERNAME
+    ),
+    "username" => "YOUR_MAIL_USERNAME",
+    "password" => "YOUR_MAIL_PASSWORD",
+    "encryption" => "YOUR_MAIL_ENCRYPTION", // E.G.: SSL/TLS/...
+);
